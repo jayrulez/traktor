3fb8b72fc19f75d622cf2ecc294cd8beb466e979
Robert Campbell <waprave@gmail.com>
2024-10-24 12:11:25 -0400

    RenderInterface stubs

--- code/RmlUi/Backend/RenderInterface.cpp
+++ code/RmlUi/Backend/RenderInterface.cpp
@@ -13,4 +13,39 @@
 namespace traktor::rmlui
 {
 	T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.rmlui.RenderInterface", 0, RenderInterface, Object)
+
+		Rml::CompiledGeometryHandle RenderInterface::CompileGeometry(Rml::Span<const Rml::Vertex> vertices, Rml::Span<const int> indices)
+	{
+		return { };
+	}
+
+	void RenderInterface::RenderGeometry(Rml::CompiledGeometryHandle geometry, Rml::Vector2f translation, Rml::TextureHandle texture)
+	{
+	}
+
+	void RenderInterface::ReleaseGeometry(Rml::CompiledGeometryHandle geometry)
+	{
+	}
+
+	Rml::TextureHandle RenderInterface::LoadTexture(Rml::Vector2i& texture_dimensions, const Rml::String& source)
+	{
+		return { };
+	}
+
+	Rml::TextureHandle RenderInterface::GenerateTexture(Rml::Span<const Rml::byte> source, Rml::Vector2i source_dimensions)
+	{
+		return { };
+	}
+
+	void RenderInterface::ReleaseTexture(Rml::TextureHandle texture)
+	{
+	}
+
+	void RenderInterface::EnableScissorRegion(bool enable)
+	{
+	}
+
+	void RenderInterface::SetScissorRegion(Rml::Rectanglei region)
+	{
+	}
 }
