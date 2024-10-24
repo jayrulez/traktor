b2ffa4bfc65fc4e4b1732b66d1eac7f0ebac3f43
Robert Campbell <waprave@gmail.com>
2024-10-24 12:11:46 -0400

    Added RmlUi singleton

---
+++ code/RmlUi/Backend/RmlUi.h
@@ -0,0 +1,67 @@
+/*
+ * TRAKTOR
+ * Copyright (c) 2022 Anders Pistol.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+#pragma once
+
+#include "Core/Config.h"
+#include "Core/Object.h"
+#include "Core/Singleton/ISingleton.h"
+#include "RmlUi/Core/Core.h"
+#include "RmlUi/Core/Input.h"
+#include "RmlUi/Backend/FileInterface.h"
+#include "RmlUi/Backend/RenderInterface.h"
+#include "RmlUi/Backend/SystemInterface.h"
+
+ // import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_RMLUI_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+using KeyDownCallback = bool (*)(Rml::Context* context, Rml::Input::KeyIdentifier key, int key_modifier, float native_dp_ratio, bool priority);
+
+namespace traktor::rmlui
+{
+	class T_DLLCLASS RmlUi
+		: public Object
+		, public ISingleton
+	{
+		T_RTTI_CLASS;
+
+	public:
+		RmlUi() = default;
+
+		static RmlUi& getInstance();
+
+		void destroy() override;
+
+		bool Initialize();
+
+		void Shutdown();
+
+		Rml::SystemInterface* GetSystemInterface();
+
+		Rml::RenderInterface* GetRenderInterface();
+
+		Rml::FileInterface* GetFileInterface();
+
+	private:
+		struct BackendData
+		{
+			FileInterface* m_fileInterface;
+			RenderInterface* m_renderInterface;
+			SystemInterface* m_systemInterface;
+		};
+
+		bool m_initialized = false;
+		BackendData* m_backendData;
+	};
+
+}
